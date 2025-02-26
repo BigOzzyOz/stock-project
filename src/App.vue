749233082377adece94cf1584ba3c91f12fa6fd7
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-02-26 23:39:06 +0100

    cleared project add background

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Stock Project</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100dvh;
+  width: 100dvw;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 0%, #011f35 100%);
+  color: white;
 }
 </style>
