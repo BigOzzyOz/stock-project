44d74355847f52da6a8231e559b4ffa46d1fe632
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-02-26 23:59:37 +0100

    add card component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <h1>Stock Project</h1>
+  <CardComponent>
+    <h2>Card</h2>
+    <p>lorem ipsum</p>
+  </CardComponent>
 </template>
 
 <script>
+import CardComponent from "./components/card.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    CardComponent,
+  },
 };
 </script>
 
@@ -16,10 +24,15 @@ export default {
   box-sizing: border-box;
 }
 
-#app {
+body {
   height: 100dvh;
   width: 100dvw;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 0%, #011f35 100%);
+  padding: 10%;
   color: white;
+}
+
+#app {
+  display: grid;
 }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+};
+</script>
+
+<style scoped>
+.card {
+  background: #011f35;
+  padding: 24px 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  border-radius: 16px;
+}
+</style>
